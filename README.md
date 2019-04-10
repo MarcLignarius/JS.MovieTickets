bd26a962a450a1c62585444e9b59b34126994659
Marc Davies <marcdaviesriot@gmail.com>
2019-04-10 02:43:40 -0700

    in progress, price not adding up and README to be complete

---
+++ README.md
@@ -0,0 +1,41 @@
+# _Address Book_
+
+#### _Epidocus Intro to Programming Week 4, 04/08/2019_
+
+#### By _**Marc Davies**_
+
+## Description
+
+_Use of Javascript objects and event delegation to build an Address Book_
+
+## Setup/Installation Requirements
+
+* _Clone from GitHub_
+* _Open index.html_
+ * _Alternatively, launch [https://marclignarius.github.io/addressbook](https://marclignarius.github.io/addressbook/)_
+
+## Specs
+
+| Behavior | Input | Output |
+| ------------- |:-------------:| -----:|
+| Program tracks a user's first name, last name and phone number | John Doe 123-456-7890 | John Doe |
+| Program displays contact information upon click | John Doe (click) | First Name: John, Last Name: Doe, Phone Number: 123-456-7890 |
+| Program deletes a contact upon click | John Doe / Delete | true |
+
+## Known Bugs
+
+_None as of last update_
+
+## Support and contact details
+
+_Please contact me at marcdaviesriot@gmail.com if you run into any issues or have questions, ideas or feedback._
+
+## Technologies Used
+
+_HTML, CSS, JS, Bootstrap, jQuery, Atom._
+
+### License
+
+*This software is licensed under the GPL license.*
+
+Copyright (c) 2019 **_Marc Davies_**
---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+// Business Logic for TicketPrice ----------------------------------------------
+// function TicketPrice() {
+//   this.ticketDetails = [];
+// }
+
+function Ticket(movie, time, age) {
+  this.movie = movie,
+  this.time = time,
+  this.age = age,
+  this.ticket = 8
+}
+
+Ticket.prototype.getTicketPrice = function (ticketDetail) {
+  if(this.movie === "Dumb and Dumber"){
+    this.ticket += 2
+  } else if(this.movie === "This Is Spinal Tap"){
+    this.ticket += 0
+  } else if(this.movie === "Rounders"){
+      this.ticket += 0
+  }
+  if(this.time === "12pm"){
+    this.ticket -= 2;
+  } else if (this.time === "4:30pm") {
+    this.ticket += 0;
+  } else if (this.time === "9:00pm") {
+    this.ticket += 2;
+  }
+  if (this.age === "child") {
+    this.ticket += 0
+  } else if (this.age === "adult") {
+    this.ticket += 2;
+  } else if (this.age === "senior") {
+    this.ticket -= 2;
+  }
+  // this.ticketDetails.push(ticketDetail);
+};
+
+// User Interface Logic --------------------------------------------------------
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    var movie = $("#movie-select").val();
+    var time = $("#time-select").val();
+    var age = $("#age-select").val();
+    var ticket = new Ticket(movie, time, age);
+    console.log(ticket);
+    var ticketArray = (Object.values(ticket));
+    $(".movietitle").text(ticketArray[0]);
+    $(".movietime").text(ticketArray[1]);
+    $(".tickettype").text(ticketArray[2]);
+    $(".ticketprice").text(ticketArray[3]);
+    $("#output").show();
+  });
+});
